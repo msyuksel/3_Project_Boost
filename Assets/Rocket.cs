1f06cdf9428c7b19fca6371d57b5abb2a166f304
msenih <senih1999@gmail.com>
2020-10-02 13:31:48 -0500

    created rocket, and added controls to it

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W)) // can thrust while rotating
+        {
+            print("thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotating right");
+        }
+    }
+}
